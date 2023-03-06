45d20762a0af992b56c2e1834c0922c3ce5f2647
dbarkowsky <dylanbarkowsky@gmail.com>
2023-03-06 13:17:20 -0800

    Apollo/graphql working in front end

--- app/composites/FormTable.js
+++ app/composites/FormTable.js
@@ -2,18 +2,35 @@ import MessageTable from "@/components/MessageTable";
 import NewMessageForm from "@/components/NewMessageForm";
 import { useState } from 'react';
 import axios from 'axios';
+import { gql, useMutation } from '@apollo/client';
 
 const FormTable = ({jsonData}) => {
+    const ADD_MESSAGE = gql`
+        mutation AddMessage($message: MessageInput) {
+            addMessage(message: $message) {
+                id
+                name
+                msgText
+            }
+        }
+    `;
 
-    const [data, setData] = useState(jsonData);
+    const [messages, setMessages] = useState(jsonData);
+    const [ addMessage ] = useMutation(ADD_MESSAGE);
 
     const addNewMessage = async (values) => {
         try {
-            let response = await axios.post(`${process.env.NEXT_PUBLIC_HOST}/api/messages`, values);
-            if (response.status == 201){
-                console.log(response);
-                setData([response.data, ...data]);
-            }
+            // let response = await axios.post(`${process.env.NEXT_PUBLIC_HOST}/api/messages`, values);
+            // if (response.status == 201){
+            //     console.log(response);
+            //     setData([response.data, ...data]);
+            // }
+            addMessage({
+                variables: { message: values },
+                onCompleted: (data) => {
+                    setMessages( [ data.addMessage, ...messages ] );
+                },
+            });
         } catch (e) {
             console.error(e);
         }
@@ -22,7 +39,7 @@ const FormTable = ({jsonData}) => {
     return (
         <>
             <NewMessageForm addNewMessage={addNewMessage} />
-            <MessageTable {...{ data }}/>
+            <MessageTable data={ messages }/>
         </>
     );
 }
--- app/pages/index.js
+++ app/pages/index.js
@@ -4,14 +4,26 @@ import PageHeader from "@/components/PageHeader";
 import PageFooter from "@/components/PageFooter";
 import { Container, Row, Col } from "react-bootstrap";
 import FormTable from "@/composites/FormTable";
-import axios from 'axios'
+import axios from 'axios';
+import { gql } from '@apollo/client';
+import client from '../apollo-client';
 
 export async function getStaticProps() {
   let jsonData;
   try {
-    const { data } = await
-      axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/messages`);
-    jsonData = data;
+    // const { data } = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/messages`);
+    const { data } = await client.query({
+      query: gql`
+        query GetMessages {
+          messages {
+            id
+            name
+            msgText
+          }
+        }
+      `
+    })
+    jsonData = data.messages;
   } catch (error) {
     console.log('API Error: ' + error);
   }
