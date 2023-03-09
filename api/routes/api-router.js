853d8fc446319ffe2e167f823d520176a679bba2
dbarkowsky <dylanbarkowsky@gmail.com>
2023-03-09 13:20:52 -0800

    protected POST for /messages

--- api/routes/api-router.js
+++ api/routes/api-router.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import { getAllMessages, addNewMessage } from '../controllers/msg-api-controller.js';
 import { registerNewUser } from '../controllers/user-api-controller.js';
+import passport from 'passport';
 
 const router = express.Router();
 
 router.route('/messages')
     .get(getAllMessages)
-    .post(addNewMessage);
+    .post(
+        passport.authenticate('basic', { session: false }),
+        addNewMessage
+    );
 
 router.route('/users')
     .post(registerNewUser);
