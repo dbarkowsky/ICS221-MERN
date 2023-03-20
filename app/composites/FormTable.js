28c115bcf283bcb00eafe5520753ef405de54a61
dbarkowsky <dylanbarkowsky@gmail.com>
2023-03-20 09:08:34 -0700

    Saved username from token for use in form

--- app/composites/FormTable.js
+++ app/composites/FormTable.js
@@ -1,19 +1,24 @@
 import MessageTable from "@/components/MessageTable";
 import NewMessageForm from "@/components/NewMessageForm";
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import axios from 'axios';
 import LoginForm from "@/components/LoginForm";
+import jwt_decode from 'jwt-decode';
 
 const FormTable = ({jsonData}) => {
 
     const [data, setData] = useState(jsonData);
     const [userAuthenticated, setUserAuthenticated] = useState(false);
+    const usernameRef = useRef(null);
 
     const logInUser = async (values) => {
+        
         let response;
         try {
             response = await axios.post(`${process.env.NEXT_PUBLIC_HOST}/api/login`, values);
             if (response.status == 200){
+                const decodedToken = jwt_decode(response.data.token);
+                usernameRef.current = decodedToken.username;
                 console.log(response);
                 sessionStorage.setItem('token', response.data.token);
                 setUserAuthenticated(true);
@@ -25,6 +30,8 @@ const FormTable = ({jsonData}) => {
     }
 
     const addNewMessage = async (values) => {
+        // add username to values object
+        values.name = usernameRef.current;
         // config for axios to use bearer token auth
         const axiosReqConfig = {
             url: `${process.env.NEXT_PUBLIC_HOST}/api/messages`,
